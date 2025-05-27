8182e664a77d10c68f36fb10439be49e3c32034c
diogodeluca <diogo@dbsolucoes.net.br>
2025-05-27 16:11:19 -0300

    correção login 07

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'http://backend:3001/api',
   headers: {
     'Content-Type': 'application/json'
   }
